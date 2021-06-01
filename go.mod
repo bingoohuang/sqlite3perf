6692e9a7f6ce1baf1702f7a1216a34a6f0e5d5c0
bingoo <bingoo.huang@gmail.com>
2021-06-01 23:45:12 +0800

    logline

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/bingoohuang/sqlite3perf
 go 1.14
 
 require (
+	github.com/bingoohuang/gg v0.0.0-20210601154318-ff90b4f03d23
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/m1ome/randstr v0.0.0-20170328115817-50e7f2dc0288
-	github.com/mattn/go-sqlite3 v1.14.0
+	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.7.0
 	github.com/valyala/fastrand v1.0.0
 	go.uber.org/atomic v1.4.0
-	golang.org/x/text v0.3.3
 )
--- logline.go
+++ logline.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/bingoohuang/gg/pkg/logline"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"io"
@@ -13,19 +14,23 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"unicode"
 )
 
 // ParseCmd is the struct representing parse sub-command.
 type ParseCmd struct {
-	File  string
-	Regex string
+	File         string
+	Samplee      string
+	Pattern      string
+	QuoteReplace string
+	LineStart    string
 }
 
 // nolint:gochecknoinits
 func init() {
 	c := ParseCmd{}
 	cmd := &cobra.Command{
-		Use:   "parse",
+		Use:   "logline",
 		Short: "parse input by pattern",
 		Long:  `parse the input file by pattern and save group values into table`,
 		Run:   c.run,
@@ -37,38 +42,43 @@ func init() {
 func (g *ParseCmd) initFlags(f *pflag.FlagSet) {
 	// Here you will define your flags and configuration settings.
 	f.StringVarP(&g.File, "file", "f", "", "file to parse")
-	f.StringVarP(&g.Regex, "regex", "r", "", "file to parse")
+	f.StringVarP(&g.Samplee, "samplee", "", "", "samplee, eg: 2021/05/29 13:09:46 Replay POST   http://1.2.3.4/solr/zz/update?v=2, cost 1.256731096s , status: 200   ")
+	f.StringVarP(&g.Pattern, "pattern", "", "", "pattern, eg: time               ########method#path|path                        #######cost|duration##########status")
+	f.StringVarP(&g.QuoteReplace, "quote", "", "\"", "quote replacement")
+	f.StringVarP(&g.LineStart, "start", "", "2021/05/29 13:09:46", "line start")
 }
 
 func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	log.Printf("Parse records by config %+v", g)
 
 	log.Print("Opening database")
-	// Database Setup
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatalf("Error while opening database '%s': %s", dbPath, err.Error())
 	}
 	defer db.Close()
 
-	// 2021/05/29 13:09:46 Replay POST http://192.166.223.29:9090/solr/licenseIndex/update?wt=javabin&version=2, cost 1.256731096s, status: 200
-	re := regexp.MustCompile(`(?P<time>.{19}) Replay POST .*? cost (?P<cost>.*?), status: (?P<status>\d+)`)
+	pattern, err := logline.NewPattern(g.Samplee, g.Pattern, logline.WithReplace(g.QuoteReplace, `"`))
+	if err != nil {
+		log.Fatalf("failed to parse logline pattern: %v", err)
+	}
 
 	createTable := `create table ` + table + `(`
 	insertTable := `insert into ` + table + `(`
 	columns := 0
-	for i, name := range re.SubexpNames() {
-		if i != 0 && name != "" {
-			if name == "cost" {
-				createTable += `costi REAL,`
-				insertTable += `costi,`
-				columns++
-			}
-
-			createTable += name + ` TEXT,`
-			insertTable += name + `,`
-			columns++
+	for _, dot := range pattern.Dots {
+		if !dot.Valid() {
+			continue
 		}
+		if dot.Type == logline.Digits {
+			createTable += dot.Name + ` INTEGER,`
+		} else if dot.Type == logline.Float {
+			createTable += dot.Name + ` REAL,`
+		} else {
+			createTable += dot.Name + ` TEXT,`
+		}
+		insertTable += dot.Name + `,`
+		columns++
 	}
 	createTable = createTable[:len(createTable)-1] + `)`
 	insertTable = insertTable[:len(insertTable)-1] + `) values (` + strings.Repeat(`,?`, columns)[1:] + `)`
@@ -88,7 +98,7 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	scanner.Split(ScanLines)
+	scanner.Split(NewScanLines(g.LineStart))
 	totalRows, rows := 0, 0
 
 	start := time.Now()
@@ -96,22 +106,15 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	ctx := cmd.Context()
 	for scanner.Scan() && ctx.Err() == nil {
 		data := scanner.Bytes()
-		match := re.FindStringSubmatch(string(data))
-
-		if len(match) == 0 {
-			continue
-		}
+		m := pattern.ParseBytes(data)
 
 		result := make([]interface{}, 0, columns)
-		for i, name := range re.SubexpNames() {
-			if i != 0 && name != "" {
-				if name == "cost" {
-					duration, _ := time.ParseDuration(match[i])
-					result = append(result, duration.Seconds())
-				}
-
-				result = append(result, match[i])
+		for _, dot := range pattern.Dots {
+			if !dot.Valid() {
+				continue
 			}
+
+			result = append(result, m[dot.Name])
 		}
 		if _, err := ps.ExecContext(ctx, result...); err != nil {
 			log.Fatalf("exec error: %v", err)
@@ -131,6 +134,22 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	Printf("Rows %d insertted, cost %s", totalRows, time.Since(start))
 }
 
+func NewScanLines(start string) bufio.SplitFunc {
+	re := ""
+	for _, c := range start {
+		if unicode.IsDigit(c) {
+			re += `\d`
+		} else {
+			re += string(c)
+		}
+	}
+
+	startPattern := regexp.MustCompile(re)
+	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		return ScanLines(startPattern, data, atEOF)
+	}
+}
+
 var printN int
 
 func Printf(format string, a ...interface{}) {
@@ -140,19 +159,17 @@ func Printf(format string, a ...interface{}) {
 	printN, _ = fmt.Printf(format, a...)
 }
 
-var timePattern = regexp.MustCompile(`\d{4}[/-]\d\d[/-]\d\d \d\d:\d\d:\d\d `) // 2021/05/29 13:09:46
-
-func ScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+func ScanLines(startPattern *regexp.Regexp, data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if atEOF && len(data) == 0 {
 		return 0, nil, nil
 	}
 	const offset = 20
 
-	if loc := timePattern.FindSubmatchIndex(data); len(loc) > 0 {
+	if loc := startPattern.FindSubmatchIndex(data); len(loc) > 0 {
 		if loc[0] > 0 {
 			return loc[0], data[:loc[0]], nil
 		} else if loc[0] == 0 && len(data) >= offset {
-			if loc2 := timePattern.FindSubmatchIndex(data[offset:]); len(loc2) > 0 {
+			if loc2 := startPattern.FindSubmatchIndex(data[offset:]); len(loc2) > 0 {
 				i := offset + loc2[0]
 				return i, data[:i], nil
 			}
