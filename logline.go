d3f264b4c6806bc7f6667eb39d935d904772388a
bingoohuang <bingoo.huang@gmail.com>
2021-06-04 09:47:23 +0800

    multiline patterns

--- logline.go
+++ logline.go
@@ -58,28 +58,12 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	}
 	defer db.Close()
 
-	patternData, err := os.ReadFile(g.PatternFile)
+	pp, err := ParsePatterns(g.PatternFile, g.QuoteReplace)
 	if err != nil {
-		log.Fatalf("Error read file '%s': %v", g.PatternFile, err)
+		log.Fatalf("parse pattern error, %v", err)
 	}
 
-	data := string(patternData)
-	n := strings.IndexByte(data, '\n')
-	if n < 0 {
-		log.Fatalf("failed to read first line as samplee at '%s'", g.PatternFile)
-	}
-	samplee := data[:n]
-	pattern := data[n+1:]
-	if n := strings.IndexByte(pattern, '\n'); n > 0 {
-		pattern = pattern[:n]
-	}
-
-	p, err := logline.NewPattern(samplee, pattern, logline.WithReplace(g.QuoteReplace, `"`))
-	if err != nil {
-		log.Fatalf("failed to parse logline pattern: %v", err)
-	}
-
-	createTable, insertTable, columns := g.createSqls(p)
+	createTable, insertTable, columns := g.createSqls(pp)
 	if _, err := db.Exec(createTable); err != nil {
 		// maybe already created, just print error and continue.
 		log.Printf("create table %s: %s", createTable, err)
@@ -102,27 +86,41 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 
 	start := time.Now()
 	ctx := cmd.Context()
+
+	idx := 0
+	values := make(map[string]interface{})
 	for scanner.Scan() && ctx.Err() == nil {
 		line := bytes.TrimSpace(scanner.Bytes())
-		m, ok := p.ParseBytes(line)
+		m, ok := pp[idx].ParseBytes(line)
 		if !ok {
+			idx = 0
 			continue
 		}
+		merge(m, values)
 
-		result := make([]interface{}, 0, columns)
-		for _, dot := range p.Dots {
-			if dot.Valid() {
-				result = append(result, m[dot.Name])
+		idx++
+
+		if idx == len(pp) {
+			result := make([]interface{}, 0, columns)
+			for _, p := range pp {
+				for _, dot := range p.Dots {
+					if dot.Valid() {
+						result = append(result, values[dot.Name])
+					}
+				}
 			}
-		}
-		if _, err := ps.ExecContext(ctx, result...); err != nil {
-			log.Fatalf("exec error: %v", err)
-		}
-		rows++
-		totalRows++
-		if rows >= 1000 {
-			Printing("Rows %d generated, cost %s", totalRows, time.Since(start))
-			rows = 0
+			if _, err := ps.ExecContext(ctx, result...); err != nil {
+				log.Fatalf("exec error: %v", err)
+			}
+			rows++
+			totalRows++
+			if rows >= 1000 {
+				Printing("Rows %d generated, cost %s", totalRows, time.Since(start))
+				rows = 0
+			}
+
+			values = make(map[string]interface{})
+			idx = 0
 		}
 	}
 
@@ -133,26 +131,82 @@ func (g *ParseCmd) run(cmd *cobra.Command, args []string) {
 	PrintEnd("Rows %d generated, cost %s\n", totalRows, time.Since(start))
 }
 
-func (g *ParseCmd) createSqls(p *logline.Pattern) (createTable, insertTable string, columns int) {
-	createTable = `create table ` + table + `(`
-	insertTable = `insert into ` + table + `(`
-	for _, dot := range p.Dots {
-		if !dot.Valid() {
+func merge(src, dst map[string]interface{}) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+func ParsePatterns(patternFile string, quoteReplace string) ([]*logline.Pattern, error) {
+	patternData, err := os.ReadFile(patternFile)
+	if err != nil {
+		return nil, fmt.Errorf("read file %s: %w", patternFile, err)
+	}
+
+	data := string(patternData)
+	lines := strings.Split(data, "\n")
+	filteredLines := make([]string, 0, len(lines))
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "--") {
 			continue
 		}
-		switch dot.Type {
-		case logline.Digits:
-			createTable += dot.Name + ` INTEGER,`
-		case logline.Float:
-			createTable += dot.Name + ` REAL,`
-		default:
-			createTable += dot.Name + ` TEXT,`
+
+		filteredLines = append(filteredLines, line)
+	}
+
+	if len(filteredLines) == 0 {
+		return nil, fmt.Errorf("at least one samplee line and one pattern line")
+	} else if len(filteredLines)%2 != 0 {
+		return nil, fmt.Errorf("pattern line should match with samplee line")
+	}
+
+	var optionFns []logline.OptionFn
+	if quoteReplace != "" {
+		optionFns = append(optionFns, logline.WithReplace(quoteReplace, `"`))
+	}
+
+	patterns := make([]*logline.Pattern, 0, len(filteredLines)/2)
+	for i := 0; i+1 < len(filteredLines); i += 2 {
+		samplee := filteredLines[i]
+		pattern := filteredLines[i+1]
+		p, err := logline.NewPattern(samplee, pattern, optionFns...)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse logline pattern: %w", err)
 		}
-		insertTable += dot.Name + `,`
-		columns++
+
+		patterns = append(patterns, p)
+	}
+
+	return patterns, nil
+}
+
+func (g *ParseCmd) createSqls(pp []*logline.Pattern) (createTable, insertTable string, columns int) {
+	createTable = `CREATE TABLE ` + table + `(`
+	insertTable = `REPLACE INTO ` + table + `(`
+
+	for _, p := range pp {
+		for _, dot := range p.Dots {
+			if !dot.Valid() {
+				continue
+			}
+			switch dot.Type {
+			case logline.Digits:
+				createTable += dot.Name + ` INTEGER`
+			case logline.Float:
+				createTable += dot.Name + ` REAL`
+			default:
+				createTable += dot.Name + ` TEXT`
+			}
+			if strings.EqualFold(dot.Name, "id") {
+				createTable += " PRIMARY KEY"
+			}
+			createTable += ","
+			insertTable += dot.Name + `,`
+			columns++
+		}
 	}
 	createTable = createTable[:len(createTable)-1] + `)`
-	insertTable = insertTable[:len(insertTable)-1] + `) values (` + strings.Repeat(`,?`, columns)[1:] + `)`
+	insertTable = insertTable[:len(insertTable)-1] + `) VALUES (` + strings.Repeat(`,?`, columns)[1:] + `)`
 	return createTable, insertTable, columns
 }
 
